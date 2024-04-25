3745cfb534d974236676207f5cc44655edc4df69
haquocdat543 <wwwdatha543@gmail.com>
2024-04-25 11:28:46 +0700

    Seperation standard backend - output

--- standard/main.tf
+++ standard/main.tf
@@ -33,11 +33,3 @@ resource "aws_resourcegroups_group" "resourcegroups_group" {
   }
 }
 
-output "config" {
-  value = {
-    bucket         = aws_s3_bucket.s3_bucket.bucket
-    region         = data.aws_region.current.name
-    role_arn       = aws_iam_role.iam_role.arn
-    dynamodb_table = aws_dynamodb_table.dynamodb_table.name
-  }
-}
